c7f6f69564f7736a81630dd7c20fccb5bfc8f305
Mr-Geekman <bunin260200@mail.ru>
2018-04-23 16:34:08 +0300

    Correct statistics calculating

--- Tester.cpp
+++ Tester.cpp
@@ -14,36 +14,38 @@ Tester::Tester(int from, int to, int num_of_repetitions) {
         test_with_points(i, num_of_repetitions);
     }
 }
-std::vector<double> Tester::get_average_differences() const {
-    return average_differences;
+std::vector<double> Tester::get_average_factors() const {
+    return average_factors;
 }
-std::vector<double> Tester::get_standart_difference_deviations() const {
-    return standart_difference_deviations;
+std::vector<double> Tester::get_standart_factor_deviations() const {
+    return standart_factor_deviations;
 }
 void Tester::draw_statistics() const {
     for(int i = from; i <= to; ++i) {
-        std::cout << i << ": " << "average difference = " << average_differences[i - from] << " standart deviation = " << standart_difference_deviations[i - from] << std::endl;
+        std::cout.fixed;
+        std::cout.precision(4);
+        std::cout << i << ": " << "average factor = " << average_factors[i - from] << " standart deviation = " << standart_factor_deviations[i - from] << std::endl;
     }
 }
 void Tester::test_with_points(int num_of_points, int num_of_repetitions) {
     PointsGenerator gen(num_of_points);
-    std::vector<double> differences(num_of_repetitions, 0.0);
-    double sum_difference = 0;
+    std::vector<double> factors(num_of_repetitions);
+    double sum_factors = 0;
     for(int i = 0; i < num_of_repetitions; ++i) {
         auto points = gen.get_points();
         gen.generate_points(num_of_points);
         MSTTravellingSalesmanProblem MST_solution(points);
         TravellingSalesmanProblem brute_solution(points);
-        differences[i] = MST_solution.get_distance() - brute_solution.get_distance();
-        sum_difference += differences[i];
+        factors[i] = MST_solution.get_distance() / brute_solution.get_distance();
+        sum_factors += factors[i];
     }
-    average_differences.push_back(sum_difference / num_of_repetitions);
+    average_factors.push_back(sum_factors / num_of_repetitions);
     // Рассчитаем стандартное отклонение
-    double average_difference = sum_difference / num_of_repetitions;
+    double average_difference = sum_factors / num_of_repetitions;
     double standart_deviation = 0;
-    for(auto value: differences) {
+    for(auto value: factors) {
         standart_deviation += pow(value - average_difference, 2);
     }
     standart_deviation = sqrt(standart_deviation / num_of_repetitions);
-    standart_difference_deviations.push_back(standart_deviation);
+    standart_factor_deviations.push_back(standart_deviation);
 }
